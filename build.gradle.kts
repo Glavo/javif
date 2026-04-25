9ca3be8aec6cdfc36cfa303490c6c58af76e49b3
Glavo <zjx001202@gmail.com>
2026-04-25 14:11:18 +0800

    Add platform-specific JavaFX dependencies to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,37 @@ repositories {
 }
 
 dependencies {
+    val osName = System.getProperty("os.name").lowercase()
+    val osArch = System.getProperty("os.arch").lowercase()
+
+    val javafxVersion = "21.0.10"
+    val javafxOS = when {
+        osName.contains("win") -> "win"
+        osName.contains("mac") -> "mac"
+        osName.contains("linux") -> "linux"
+        else -> null
+    }
+    val javafxArch = when (osArch) {
+        "amd64", "x86-64", "x64" -> ""
+        "aarch64", "arm64" -> "-aarch64"
+        else -> null
+    }
+
+    fun javafx(module: String) {
+        if (javafxOS != null && javafxArch != null) {
+            val notation = "org.openjfx:javafx-$module:$javafxVersion:${javafxOS}${javafxArch}"
+
+            compileOnly(notation)
+            testCompileOnly(notation)
+            testRuntimeOnly(notation)
+        }
+    }
+
+    javafx("base")
+    javafx("controls")
+    javafx("graphics")
+    javafx("swing") // For Benchmark
+
     compileOnlyApi("org.jetbrains:annotations:26.1.0")
 
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
